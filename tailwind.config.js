ff1a1b1ecdf432cc282889d508176e4b5116a9e1
Finn <fritzen@vmd128257.contaboserver.net>
2024-05-23 18:46:37 +0200

    initial content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
   theme: {
     extend: {},
+    screens: {
+      'xsm': '400px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+
+    }
   },
   plugins: [],
 }
